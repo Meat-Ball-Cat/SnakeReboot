bb78b5cf381bb1840be9d93cf12a5716523eb246
InnocentKhajiit <alekses2001@gmail.com>
2021-04-02 13:25:45 +0500

    Добавьте файлы проекта.

---
+++ ToolsLibrary/KeyPress.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TolsLibrary
+{
+    public static class KeyPress
+    {
+        static Thread keyPressThread;
+        static Dictionary<ConsoleKey, EventHandler> keyPressEvent = new Dictionary<ConsoleKey, EventHandler>();
+        public static void Start()
+        {
+            keyPressThread = new Thread(WaitingForPress);
+            keyPressThread.Start();
+        }
+        public static void Close()
+        {
+            if (keyPressThread.IsAlive)
+            {
+                keyPressThread.Abort();
+            }
+        }
+        static void WaitingForPress()
+        {
+            while (true)
+            {
+                var PressedKey = Console.ReadKey(true);
+                keyPressEvent.TryGetValue(PressedKey.Key, out var keyEventHandler);
+                keyEventHandler?.Invoke(PressedKey, new EventArgs());
+            }
+        }
+        public static bool Set(ConsoleKey keyValue, EventHandler action)
+        {
+            try
+            {
+                if (!keyPressEvent.ContainsKey(keyValue))
+                {
+                    keyPressEvent.Add(keyValue, action);
+                    return true;
+                }
+                else
+                {
+                    return Reset(keyValue, action);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static bool Reset(ConsoleKey keyValue, EventHandler action)
+        {
+            try
+            {
+                keyPressEvent.Remove(keyValue);
+                keyPressEvent.Add(keyValue, action);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ToolsLibrary/Сoordinates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsLibrary
+{
+    public struct coord
+    {
+        public int x { get; private set; }
+        public int y { get; private set; }
+        public coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public static coord operator +(coord x, coord y)
+        {
+            return new coord(x.x + y.x, x.y + y.y);
+        }
+        public static coord operator -(coord x, coord y)
+        {
+            return new coord(x.x - y.x, x.y - y.y);
+        }
+        public static implicit operator (int x, int y)(coord value)
+        {
+            return (value.x, value.y);
+        }
+        public static implicit operator coord((int x, int y) value)
+        {
+            return new coord(value.x, value.y);
+        }
+        public coord up() => (x, y - 1);
+        public coord down() => (x, y + 1);    
+        public coord left() => (x - 1, y);
+        public coord right() => (x + 1, y);
+    }
+}
